6540033a6b672deaf62f33bba36ac8817b445405
gemac2 <gmachacon@wawand.co>
2019-11-12 17:20:12 -0500

    fixed exercice 3

--- main.go
+++ main.go
@@ -8,14 +8,16 @@ var a, b int = 5, 4
 var number int = 0
 
 func main() {
-	fmt.Println("Exercice 1")
+	fmt.Println("Exercice 1 - Hello World")
 	fmt.Println("Hello World!")
-	fmt.Println("Exercice 2")
+	fmt.Println("Exercice 2 - Add two numbers")
 	fmt.Println(a, "+", b, "is", sum(a, b))
-	fmt.Println("Exercice 3")
+	fmt.Println("Exercice 3 - Print numbers from 1 to 10")
 	for i := 1; i <= 10; i++ {
-		fmt.Println(i)
+		number = i
+		fmt.Print(number, " ")
 	}
+
 }
 
 func sum(a, b int) int {
