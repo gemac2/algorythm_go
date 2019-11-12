f054078f1ed0d7f0071295cab6b3ce049cddc631
gemac2 <gmachacon@wawand.co>
2019-11-12 16:20:28 -0500

    print number 1-10

--- main.go
+++ main.go
@@ -5,12 +5,16 @@ import (
 )
 
 var a, b int = 5, 4
+var number int = 0
 
 func main() {
 	fmt.Println("Exercice 1")
 	fmt.Println("Hello World!")
 	fmt.Println("Exercice 2")
 	fmt.Println(a, "+", b, "is", sum(a, b))
+	for i := 1; i <= 10; i++ {
+		fmt.Println(i)
+	}
 }
 
 func sum(a, b int) int {
